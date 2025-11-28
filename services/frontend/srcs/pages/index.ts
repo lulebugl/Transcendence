4445a49d69fcb1ba5b7f71a08f1c9cdaa8dbebfa
hle-roi <hugo_leroi@yahoo.com>
2025-11-28 13:59:11 +0100

    Fix import typos

--- services/frontend/srcs/pages/index.ts
+++ services/frontend/srcs/pages/index.ts
@@ -5,4 +5,4 @@ export { Settings } from "./settings/Settings";
 export { UserProfile } from "./profile/UserProfile";
 export { Lobby } from "./pong/Lobby";
 export { Login } from "./auth/Login";
-export { Signup } from "./auth/Signup";
+export { Signup } from "./auth/SignUp";
